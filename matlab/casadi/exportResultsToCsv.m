bf2585139cc845423a2ebd9e85a711ed922f4714
Lorenzo <lorenzo.moretti@iit.it>
2022-12-22 17:04:28 +0100

    exporting also contact forces at CoP for both foot

--- matlab/casadi/exportResultsToCsv.m
+++ matlab/casadi/exportResultsToCsv.m
@@ -1,4 +1,4 @@
-function exportResultsToCsv(xsol, t_sol, activeFeet, feetLocations)
+function exportResultsToCsv(xsol, usol, t_sol, activeFeet, feetLocations)
 
 mass = 1.5; %[kg]
 
@@ -35,16 +35,38 @@ foot_location_left = [foot_location_left(1,:);
 foot_location_right = reshape(cell2mat(feetLocations(:,2)),3,[])';
 foot_location_right = [foot_location_right(1,:);
     foot_location_right];
-x_foot_location_left = interp1(timings, foot_location_left(:,1), t, "next");
-y_foot_location_left = interp1(timings, foot_location_left(:,2), t, "next");
-z_foot_location_left = interp1(timings, foot_location_left(:,3), t, "next");
-x_foot_location_right = interp1(timings, foot_location_right(:,1), t, "next");
-y_foot_location_right = interp1(timings, foot_location_right(:,2), t, "next");
-z_foot_location_right = interp1(timings, foot_location_right(:,3), t, "next");
+
+foot_location_left = interp1(timings, foot_location_left, t, "next");
+foot_location_right = interp1(timings, foot_location_right, t, "next");
+
+x_foot_location_left = foot_location_left(:,1);
+y_foot_location_left = foot_location_left(:,2);
+z_foot_location_left = foot_location_left(:,3);
+x_foot_location_right = foot_location_right(:,1);
+y_foot_location_right = foot_location_right(:,2);
+z_foot_location_right = foot_location_right(:,3);
+
+%CoP
+CoP_left = vertcat(usol(1:2,:), zeros(1,size(usol,2)));
+CoP_left = interp1(time(1:end-1)', CoP_left', t, 'linear','extrap'); 
+CoP_right = vertcat(usol(4:5,:),zeros(1,size(usol,2)));
+CoP_right = interp1(time(1:end-1)', CoP_right', t, 'linear','extrap'); 
+
+%export forces (at CoP);  
+COM = [xCOM, yCOM, zCOM];
+mass = 1.5; %[kg]
+R = eye(3); %foot is alligned with inertial frame
+u_left = interp1(time(1:end-1)', usol(3,:)', t, 'linear','extrap'); % left force multiplier 
+u_right = interp1(time(1:end-1)', usol(6,:)', t, 'linear','extrap'); % right force multiplier 
+F_left = mass * repmat(u_left,1,3) .* (COM-(foot_location_left+transpose(R*CoP_left')));
+F_right = mass * repmat(u_right,1,3) .* (COM-(foot_location_right+transpose(R*CoP_right')));
 
 % write header
 filename = 'TestTrajectory.csv';
-header_string = 'time, contact_status_left, contact_status_right, pc-x_left, pc-y_left, pc-z_left, pc-x_right, pc-y_right, pc-z_right, x-COM, y-COM, z-COM, h_x, h_y, h_z';
+header_string = ['time, contact_status_left, contact_status_right,',...
+                'pc-x_left, pc-y_left, pc-z_left, pc-x_right, pc-y_right, pc-z_right,'...
+                'x-COM, y-COM, z-COM, h_x, h_y, h_z,',...
+                'Fx-left, Fy-left, Fz-left, Fx-right, Fy-right, Fz-right'];
 fid = fopen(filename,'w');
 fprintf(fid,'%s\r\n',header_string);
 fclose(fid);
@@ -53,7 +75,7 @@ fclose(fid);
 m = [t, contact_flag_left, contact_flag_right, ...
     x_foot_location_left, y_foot_location_left, z_foot_location_left, ...
     x_foot_location_right, y_foot_location_right, z_foot_location_right, ...
-    xCOM, yCOM, zCOM, hxCOM, hyCOM, hzCOM];
+    xCOM, yCOM, zCOM, hxCOM, hyCOM, hzCOM, F_left, F_right];
 
 dlmwrite(filename, m,'-append','delimiter',',');
 end
